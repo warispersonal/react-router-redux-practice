6c51a5139ed60443fffed9b16310a93d6fdb4c6b
warispersonal <wariszargarpersonal@gmail.com>
2021-11-17 21:54:00 +0500

    multiple .env file

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -3,10 +3,11 @@ import ProgmaticallyChangeRoute from "../ProgmaticallyChangeRoute/ProgmaticallyC
 
 class Home extends Component {
     render() {
-        const  {REACT_APP_SECRET_NAME, REACT_APP_TITLE} = process.env
+        const  {REACT_APP_SECRET_NAME, REACT_APP_TITLE, REACT_APP_API_URL} = process.env
         return (
             <div>
                 <h1>{REACT_APP_TITLE}</h1>
+                <h1>{REACT_APP_API_URL}</h1>
                 <p style={{textAlign: 'center'}}>{REACT_APP_SECRET_NAME}</p>
                 <ProgmaticallyChangeRoute />
             </div>
