9bb4b0cfe5543acbeff577be4ee22e9bad834fd3
warispersonal <wariszargarpersonal@gmail.com>
2021-11-17 21:12:37 +0500

    how to change default port & how to use .env file

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -3,9 +3,11 @@ import ProgmaticallyChangeRoute from "../ProgmaticallyChangeRoute/ProgmaticallyC
 
 class Home extends Component {
     render() {
+        const  {REACT_APP_SECRET_NAME, REACT_APP_TITLE} = process.env
         return (
             <div>
-                <h1>Home Page</h1>
+                <h1>{REACT_APP_TITLE}</h1>
+                <p style={{textAlign: 'center'}}>{REACT_APP_SECRET_NAME}</p>
                 <ProgmaticallyChangeRoute />
             </div>
         );
